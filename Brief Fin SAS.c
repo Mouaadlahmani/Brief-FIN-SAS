5dededbce2f4d8e3c6b1c31a6764af541f3f42b9
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-01-31 09:24:17 +0100

    +Filtrer

--- Brief Fin SAS.c
+++ Brief Fin SAS.c
@@ -1,126 +1,177 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-//structure pour stocker les informations d'une tache
-struct tache{
-    char description[100];
-    char date[20];
-    int priorite;
+// structure pour stocker la date d'echeance d'une tache
+struct date_d {
+  int jour;
+  int mois;
+  int annee;
+};
+// structure pour stocker les informations d'une tache
+struct tache {
+  char nom[20];
+  char description[100];
+  int priorite;
+  struct date_d date;
 };
 
-//tableau pour stocker les taches
-    struct tache liste[100];
-    int tachenombre = 0;
+// tableau pour stocker les taches
+struct tache liste[100];
+int tachenombre = 0;
 
-//fonction pour ajouter une tache
-void ajouter(){
-    struct tache nvtache;
-    
-    printf("Description de la tache : ");
-    scanf(" %[^\n]",&nvtache.description);
+// fonction pour ajouter une tache
+void ajouter() {
+  struct tache nvtache;
 
-    printf("Date d'echeance (format : jj/mm/aaaa) : ");
-    scanf("%s",&nvtache.date);
+  printf("Nom de la tache : ");
+  scanf(" %[^\n]s", &nvtache.nom);
 
-    printf("Priorite (1 ou 2) : ");
-    scanf("%d",&nvtache.priorite);
+  printf("Description de la tache : ");
+  scanf(" %[^\n]s", &nvtache.description);
 
-    liste[tachenombre++] = nvtache;
+  printf("La priorite (1.Haut | 2.Moyenne | 3.Faible) : ");
+  scanf("%d", &nvtache.priorite);
 
-    printf("Tache ajoutee avec succees. \n");
+  printf("La date d'echeance : \n");
+  printf("Jour : ");
+  scanf("%d", &nvtache.date.jour);
+  printf("Mois : ");
+  scanf("%d", &nvtache.date.mois);
+  printf("Annee : ");
+  scanf("%d", &nvtache.date.annee);
+
+  liste[tachenombre++] = nvtache;
+  printf("Tache ajoutee avec succes. \n");
 }
 
-//fonction pour afficher la liste des taches
-void afficher(){
-    int i;
-   if(tachenombre == 0){
+// fonction pour afficher la liste des taches
+void afficher() {
+  int i;
+  if (tachenombre == 0) {
     printf("Aucune tache a afficher. \n");
+  } else {
+    printf("\nLa liste des taches : \n");
+    for (i = 0; i < tachenombre; i++) {
+      printf("\nTache N%d: \n", i + 1);
+      printf("Nom : %s \n", liste[i].nom);
+      printf("Description : %s \n", liste[i].description);
+      printf("Priorite : %d \n", liste[i].priorite);
+      printf("Date d'echeance : %d/%d/%d \n\n", liste[i].date.jour,
+             liste[i].date.mois, liste[i].date.annee);
+      printf("----------------------------- \n");
     }
-    else{
-        printf("La liste des taches : \n");
-        for(i=0;i<tachenombre;i++){
-            printf("Tache : %d \n",i+1);
-            printf("Decription : %s \n",liste[i].description);
-            printf("Date d'echeance : %s \n",liste[i].date);
-            printf("Priorite : %d \n",liste[i].priorite);
-        }
-    }
+  }
 }
 
-//fonction pour modiffier une tache
-void modiffier(){
-    int tacheindice;
-    
-    afficher();
-    
-    printf("Quelle tache voulez vous modiffier : ");
-    scanf("%d",&tacheindice);
+// fonction pour modiffier une tache
+void modiffier() {
+  int tacheindice;
 
-    if(tacheindice>=1 && tacheindice<=tachenombre){
+  afficher();
+  printf("Quelle tache voulez vous modiffier : ");
+  scanf("%d", &tacheindice);
 
-       printf("Nouvelle description : ");
-       scanf(" %[^\n]",&liste[tacheindice-1].description);
+  if (tacheindice >= 1 && tacheindice <= tachenombre) {
 
-       printf("La vouvelle date d'echeance : ");
-       scanf("%s",&liste[tacheindice-1].date);
+    printf("Nouveau nom de tache : ");
+    scanf(" %[^\n]", &liste[tacheindice - 1].nom);
 
-       printf("La nouvelle priorite : ");
-       scanf("%d",&liste[tacheindice-1].priorite);
+    printf("Nouvelle description : ");
+    scanf(" %[^\n]", &liste[tacheindice - 1].description);
 
-       printf("Tache modiffiee avec succees. \n");
-    }
-    else{
-        printf("Numero de tache invalide. \n");
-    }
+    printf("Nouvelle priorite : ");
+    scanf("%d", &liste[tacheindice - 1].priorite);
+
+    printf("Nouvelle date d'echeance : \n");
+    printf("Jour : ");
+    scanf("%d", &liste[tacheindice - 1].date.jour);
+    printf("Mois : ");
+    scanf("%d", &liste[tacheindice - 1].date.mois);
+    printf("Annee : ");
+    scanf("%d", &liste[tacheindice - 1].date.annee);
+
+    printf("Tache modiffiee avec succes. \n\n");
+  } else {
+    printf("Tache invalide. \n\n");
+  }
 }
 
-//fonction pour suprimer une tache
-void suprimer(){
-	int tacheindice,i;
-	
-	afficher();
-	
-	printf("Quelle tache voulez vous suprimer : ");
-	scanf("%d",&tacheindice);
-	
-	if(tacheindice>=1 && tacheindice<=tachenombre){
-		for(i=tacheindice-1;i<tachenombre-1;i++){
-			liste[i] = liste[i+1];
-		}
-		tachenombre--;
-		printf("Tache suprimer avec succes. \n");
-	}else{
-		printf("Numero de tache invalide. \n");
-	}
+// fonction pour suprimer une tache
+void suprimer() {
+  int tacheindice, i;
+  afficher();
+
+  printf("Quelle tache voulez vous suprimer : ");
+  scanf("%d", &tacheindice);
+
+  if (tacheindice >= 1 && tacheindice <= tachenombre) {
+    for (i = tacheindice - 1; i < tachenombre - 1; i++) {
+      liste[i] = liste[i + 1];
+    }
+    tachenombre--;
+    printf("La tache suprimee avec succes. \n\n");
+  } else {
+    printf("Tache invalide. \n\n");
+  }
 }
 
-int main()
-{
-    int choix;
-    do{
-    	printf("=====MENU===== : \n");
-        printf("1.Ajouter un tache. \n");
-        printf("2.Afficher la liste des taches. \n");
-        printf("3.Modiffier une tache. \n");
-        printf("4.Suprimer une tache. \n");
-        printf("0.Quitter. \n");
-        printf("Votre choix :");
-        scanf("%d",&choix);
-        switch(choix){
-                case 1 : ajouter();
-                        break;
-                case 2 : afficher();
-                        break;
-                case 3 : modiffier();
-                        break;
-                case 4 : suprimer();
-                        break;        
-                case 0 : printf("Le programme est termine");
-                        break;
-                default : printf("Choix invalide ,Veuillez ressayer");
-                        break;
-        }
-    }while(choix != 0);
-    return 0;
+// fonction de filtrage des taches par priorite
+void filtrer() {
+  int i, priorite;
+  printf("La priorite a filtrer (1.Haut | 2.Moyenne | 3.Faible) : ");
+  scanf("%d", &priorite);
+  if (priorite == 1 || priorite == 2 || priorite == 3) {
+    printf("\nLes taches de priorite %d : \n", priorite);
+    for (i = 0; i < tachenombre; i++) {
+      if (liste[i].priorite == priorite) {
+        printf("\nNom : %s \n", liste[i].nom);
+        printf("Description : %s \n", liste[i].description);
+        printf("Priorite : %d \n", liste[i].priorite);
+        printf("Date d'echeance : %d/%d/%d \n", liste[i].date.jour,
+               liste[i].date.mois, liste[i].date.annee);
+        printf("----------------------------- \n");
+      }
+    }
+  } else {
+    printf("Priorite invalide. \n\n");
+  }
+}
+
+int main() {
+  int choix;
+  do {
+    printf("=====MENU===== : \n");
+    printf("1.Ajouter une tache : \n");
+    printf("2.Afficher la liste des taches : \n");
+    printf("3.Modiffier une tache : \n");
+    printf("4.Suprimer une tache : \n");
+    printf("5.Filtrer les taches par priorite : \n");
+    printf("0.Quitter : \n\n");
+    printf("Votre choix : ");
+    scanf("%d", &choix);
+
+    switch (choix) {
+    case 1:
+      ajouter();
+      break;
+    case 2:
+      afficher();
+      break;
+    case 3:
+      modiffier();
+      break;
+    case 4:
+      suprimer();
+      break;
+    case 5:
+      filtrer();
+      break;
+    case 0:
+      printf("Le programme est termine");
+      break;
+    default:
+      printf("Choix invalide. Veuillez ressayer. \n");
+    }
+  } while (choix != 0);
+
+  return 0;
 }
